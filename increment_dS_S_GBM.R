6148181d7d255f2c4f29363c44ddc4e6b19371dc
pmilloss <44592045+pmilloss@users.noreply.github.com>
2019-05-09 16:20:31 +0100

    Update increment_dS_S_GBM.R

--- increment_dS_S_GBM.R
+++ increment_dS_S_GBM.R
@@ -1,6 +1,6 @@
-simula_dS_S_GBM <- function(nsim, N, r, sigma, d)
+simula_dS_S_GBM <- function(nsim, N, r, sigma)
 {
-  dW <- matrix(rnorm(nsim * N, 0, sqrt(d)), nrow = N, ncol = nsim)
-  dS_S <- exp((r - 0.5 * sigma ^ 2) * d + sigma * dW) - 1
+  dW <- matrix(rnorm(nsim * N), nrow = N, ncol = nsim)
+  dS_S <- exp((r - 0.5 * sigma ^ 2) + sigma * dW) - 1
   return(dS_S)  
 }
