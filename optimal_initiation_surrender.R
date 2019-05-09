860688716a17b5acb5ffd5963c2243e3de05c27f
pmilloss <44592045+pmilloss@users.noreply.github.com>
2019-05-09 16:21:06 +0100

    Update optimal_initiation_surrender.R

--- optimal_initiation_surrender.R
+++ optimal_initiation_surrender.R
@@ -1,4 +1,4 @@
-optimal_initiation_surrender <- function(nsim, X, M, state_var = NULL, G, basis_fun, n_basis_fun, N, d, tau, B)
+optimal_initiation_surrender <- function(nsim, X, M, state_var = NULL, G, basis_fun, Nbar, d, tau, B)
 {
   # calculate contract value at time 0 that can be initiated optimally and early surrendered
   
@@ -20,20 +20,18 @@ optimal_initiation_surrender <- function(nsim, X, M, state_var = NULL, G, basis_
   payoff <- X[cbind(tau + 1, 1 : nsim)]
   # payoff in case of death or initiation
   
-  Nbar <- max(tau)
+  # Nbar <- max(tau)
   
-  for (n in (Nbar - 1) : 0)
+  for (t in (Nbar - 1) : 0)
   {
-    ind_surv <- which(tau > n)
+    ind_surv_t <- which(tau > t)
     # (indices of) survivors
     
-    init_value_surr <- G[n + 1, ind_surv]
+    init_value_surr <- G[t + 1, ind_surv_t]
     
-    cont_value <- payoff[ind_surv] * B[n + 1, ind_surv] / B[cbind(lambda[ind_surv] + 1, ind_surv)]
+    cont_value <- payoff[ind_surv_t] * B[t + 1, ind_surv_t] / B[cbind(lambda[ind_surv_t] + 1, ind_surv_t)]
     
-    # regression_matrix[j, ] <- basis_functions(c(X[n + 1, ind_surv_j], M[n + 1, ind_surv_j])) # AGGIUNGERE ALTRE VARIABILI DI STATO
-    Z <- cbind(X[n + 1, ind_surv], M[n + 1, ind_surv])
-    # matrix of state variables
+    if(is.null(state_var)) Z <- cbind(1, X[t + 1, ind_surv_t], M[t + 1, ind_surv_t]) else Z <- cbind(1, X[t + 1, ind_surv_t], M[t + 1, ind_surv_t], sapply(state_var, extract_row_col, row = t + 1, col = ind_surv_t))
     
     reg_mat <- t(apply(Z, 1, basis_fun))
     # apply "basis_functions" to each row of Z to build the regression matrix
@@ -46,34 +44,11 @@ optimal_initiation_surrender <- function(nsim, X, M, state_var = NULL, G, basis_
     
     index_init <- (init_value_surr_LSMC > cont_value_LSMC)
     
-    payoff[ind_surv][index_init] <- init_value_surr[index_init]
-    lambda[ind_surv][index_init] <- n
+    payoff[ind_surv_t][index_init] <- init_value_surr[index_init]
+    lambda[ind_surv_t][index_init] <- t
     
   }
-  
   contract_value_0 <- mean(payoff / B[lambda + 1, ])
-  
   SE <- sd(payoff / B[lambda + 1, ]) / sqrt(nsim)
-  
   return(list(contract_value_0, SE, lambda))
 }
-
-
-# system.time(res3 <- optimal_initiation_early_surrender(nsim = nsim, X = XM[[1]], M = XM[[2]], state_variables = NULL, F = FD, basis_functions = basis_functions_2v[[3]], n_basis_functions = 9, N = 60, d = 1, tau = tau, B = B, penalty = 1))
-# 
-# system.time(res3 <- optimal_initiation_early_surrender(nsim = nsim, X = XM[[1]], M = XM[[2]], state_variables = NULL, F = FD, basis_functions = basis_functions_2v[[3]], n_basis_functions = 9, N = 60, d = 1, tau = tau, B = B, penalty = 0.2))
-# 
-# XM <- simulate_XM(nsim, X0 = 1, M0 = X0, dS_S, N, d, psi, phi = 0.03, beta)
-# 
-# FD <- calculate_F(nsim, XM[[1]], XM[[2]], dS_S, N, d, psi, phi = 0.03, beta = 0, gt = g, tau, B)
-# 
-# 
-# system.time(res4 <- optimal_initiation_early_surrender(nsim = nsim, X = XM[[1]], M = XM[[2]], state_variables = NULL, F = FD, basis_functions = basis_functions_2v[[3]], n_basis_functions = 9, N = 60, d = 1, tau = tau, B = B, penalty = 0))
-# 
-# system.time(res5 <- optimal_initiation_early_surrender(nsim = nsim, X = XM[[1]], M = XM[[2]], state_variables = NULL, F = FD, basis_functions = basis_functions_2v[[3]], n_basis_functions = 9, N = 60, d = 1, tau = tau, B = B, penalty = 0.04))
-
-
-# 
-# system.time(res2 <- optimal_initiation(nsim = nsim, X = XM[[1]], M = XM[[2]], state_variables = NULL, F = FD, basis_functions = basis_functions_2v[[3]], n_basis_functions = 9, N = 60, d = 1, tau = tau, B = B))
-
-
