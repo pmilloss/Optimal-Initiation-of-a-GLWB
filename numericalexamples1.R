114920a88ccebb7276ae52fd8b003e91c082bf71
pmilloss <44592045+pmilloss@users.noreply.github.com>
2019-05-09 16:20:39 +0100

    Update numericalexamples1.R

--- numericalexamples1.R
+++ numericalexamples1.R
@@ -1,6 +1,17 @@
+setwd("C://Dropbox//Università//Lavori//Monte Carlo valuation of the initiation option in a GLWB variable annuity//R code")
+
+source("increment_dS_S_GBM.R")
+source("simulate_tau_gompertz.R")
+source("basisfunctions.R")
+source("simulate_XM.R")
+source("calculate_F.R")
+source("calculate_G.R")
+source("optimal_initiation_no_surrender.R")
+source("optimal_initiation_early_surrender.R")
+source("optimal_initiation_surrender.R")
 # calculate fair insurance fees in the initiation only and initiation + early surrender case
 
-X0 <- 1
+X0 <- 100
 M0 <- X0
 
 r <- 0.03
@@ -11,62 +22,84 @@ phi <- 0
 
 g <- 0.05
 
-beta <- 0.05
+beta <- 0.06
 
-nsim <- 10000
-N <- 60
-d <- 1
+penalty <- 0.1
 
+nsim <- 1000
 
 set.seed(0)
-
-B <- matrix(exp(r * (0 : N)), nrow = N + 1, ncol = nsim, byrow = FALSE)
-
-dS_S <- simula_dS_S_GBM(nsim, N, r, sigma, d)
 
 x0 <- 60; m <- 87.25; b <- 9.5
 tau <- simulate_tau_gompertz(nsim = nsim, x0 = x0, b = b, m = m)
 tau <- ceiling(tau)
 
+# N <- 60
+Nbar <- max(tau)
+
+B <- matrix(exp(r * (0 : Nbar)), nrow = Nbar + 1, ncol = nsim, byrow = FALSE)
+
+dS_S <- simula_dS_S_GBM(nsim, Nbar, r, sigma)
+
+XM <- simulate_XM(nsim, X0 = 100, M0 = X0, dS_S, Nbar, psi, phi, beta)
+
+FF <- calculate_F(nsim, XM[[1]], XM[[2]], dS_S, psi, phi, beta = 0, gt = g, tau, Nbar, B)
+
+# no surrender
+VV0 <- optimal_initiation_no_surrender(nsim = nsim, X = XM[[1]], M = XM[[2]], FF = FF, basis_fun = basis_functions_2v[[3]], tau = tau, NBar = Nbar, B = B)
+
+# early surrender
+VV1 <- optimal_initiation_early_surrender(nsim = nsim, X = XM[[1]], M = XM[[2]], FF = FF, basis_fun = basis_functions_2v[[3]], tau = tau, Nbar = Nbar, B = B, penalty = penalty)
+
+GG <- calculate_G(nsim = nsim, X = XM[[1]], M = XM[[2]], dS_S =dS_S, Nbar = Nbar, psi = psi, phi = phi, beta = 0, gt = g, tau = tau, B = B, penalty = penalty, basis_fun = basis_functions_2v[[3]])
+
+# surrender
+VV2 <- optimal_initiation_surrender(nsim = nsim, X = XM[[1]], M = XM[[2]], state_var = NULL, G = GG, basis_fun = basis_functions_2v[[3]], Nbar = Nbar, tau = tau, B = B)
 
 
 
 
-fff <- function(){
 
-  fairphi <- c()
+
+
+
+
+
+
+
+
+find_fair_phi <- function(){
+
+  fairphi <- 0
   
-
   phiV <- c(0, 0.1, seq(0.01, 0.09, by = 0.01))
   VV <- vector(mode = "numeric", length = length(phiV))
     
     for (j in 1 : length(phiV))
     {
       
-      XM <- simulate_XM(nsim, X0 = 1, M0 = X0, dS_S, N, d, psi, phiV[j], beta)
+      XM <- simulate_XM(nsim, X0 = 100, M0 = X0, dS_S, Nbar, d, psi, phiV[j], beta)
       
-      FD <- calculate_F(nsim, XM[[1]], XM[[2]], dS_S, N, d, psi, phiV[j], beta = 0, gt = g, tau, B)
+      FD <- calculate_F(nsim, XM[[1]], XM[[2]], dS_S, Nbar, d, psi, phiV[j], beta = 0, gt = g, tau, B)
       
-      VV[j] <- optimal_initiation(nsim = nsim, X = XM[[1]], M = XM[[2]], state_variables = NULL, F = FD, basis_functions = basis_functions_2v[[3]], n_basis_functions = 9, N = 60, d = 1, tau = tau, B = B)[[1]]
+      VV[j] <- optimal_initiation_no_surrender(nsim = nsim, X = XM[[1]], M = XM[[2]], F = FD, basis_fun = basis_functions_2v[[3]], n_basis_fun = 9, N = Nbar, tau = tau, B = B)[[1]]
       
-      print(c(j, VV[j]))
+      print(c(j, phiV[j] * 100, VV[j]))
       
       if (j == 1 & VV[1] < X0) {fairphi <- -99; break()}
       if (j == 2 & VV[2] > X0) {fairphi <- 99; break()}
-      
-      
+  
     }
-    if (fairphi %in% c(-99, 99)) next()
-    
+    if (fairphi %in% c(-99, 99)) return(fairphi)
+  
     splineV <- function(fff)splinefun(x = phiV, y = VV, method = "hyman" )(fff) - X0
     fairphi <- uniroot(f = splineV, interval = range(phiV))$root
-    print(fairphi)
-  return(fairphi)
+    return(fairphi)
 }
 
-
-
-
+debug(optimal_initiation_no_surrender)
+undebug(find_fair_phi)
+find_fair_phi()
 
 
 
